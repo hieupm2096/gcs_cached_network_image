c377ba071888efe0606f2b156a552b373d10079c
hieu <hieupm2096@gmail.com>
2026-04-10 09:24:47 +0700

    Synchronize platform plugins and fix rendering tests
    - Add jni FFI plugin to Linux and Windows build configurations.
    - Update macOS plugin registration to use sqflite_darwin and remove path_provider.
    - Align FakeTicker implementation with updated Flutter rendering interface by adding forceFrames.

--- cached_network_image/test/rendering_tester.dart
+++ cached_network_image/test/rendering_tester.dart
@@ -311,6 +311,9 @@ class FakeTicker implements Ticker {
       style: DiagnosticsTreeStyle.errorProperty,
     );
   }
+
+  @override
+  bool forceFrames = false;
 }
 
 class TestClipPaintingContext extends PaintingContext {
